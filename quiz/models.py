c63add5405b50b9a2445c1cbbc5cfda7e1b835f4
hannacantwell <hanna.cantwell@bbm.bonnier.se>
2016-09-09 10:16:55 +0200

    make migration

--- quiz/models.py
+++ quiz/models.py
@@ -5,12 +5,12 @@ class Quiz(models.Model):
 	name = models.CharField(max_length=100)
 	description = models.TextField()
 
-	class Question(models.Model):
-		question = models.TextField()
-		answer1 = models.CharField(max_length=100)
-		answer2 = models.CharField(max_length=100)
-		answer3 = models.CharField(max_length=100)
-		correct = models.PositiveIntegerField()
-		quiz = models.ForeignKey(Quiz, related_name="questions")
+class Question(models.Model):
+	question = models.TextField()
+	answer1 = models.CharField(max_length=100)
+	answer2 = models.CharField(max_length=100)
+	answer3 = models.CharField(max_length=100)
+	correct = models.PositiveIntegerField()
+	quiz = models.ForeignKey(Quiz, related_name="questions")
 
 # Create your models here.
