6a05d8b8175c4f6018838d62e9be66263505ad27
hannacantwell <hanna.cantwell@bbm.bonnier.se>
2016-09-09 12:48:48 +0200

    added txt files in sublime

--- quiz/views.py
+++ quiz/views.py
@@ -5,13 +5,13 @@ from quiz.models import Quiz
 
 def startpage(request):
 	context = {
-	    	"quizzes": Quiz.objects.all(),
+			"quizzes": Quiz.objects.all(),
 	}
 	return render(request, "quiz/start.html", context)
 
 def quiz(request, quiz_number):
 	context = {
-	    	"quiz": Quiz.objects.get(quiz_number=quiz_number),
+			"quiz": Quiz.objects.get(quiz_number=quiz_number),
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/quiz.html", context)
@@ -21,12 +21,12 @@ def question(request, quiz_number, question_number):
 	questions = quiz.questions.all()
 	question = questions[int(question_number) - 1]
 	context = {
-    		"question_number": question_number,
-    		"question": question.question,
-	    	"answer1": question.answer1,
-    		"answer2": question.answer2,
-	    	"answer3": question.answer3,
-	    	"quiz": quiz,
+			"question_number": question_number,
+			"question": question.question,
+			"answer1": question.answer1,
+			"answer2": question.answer2,
+			"answer3": question.answer3,
+			"quiz": quiz,
 		"quiz_number": quiz_number,
 	}
 	return render(request, "quiz/question.html", context)
@@ -45,9 +45,16 @@ def answer(request, quiz_number, question_number):
 		return redirect("question_page", quiz_number, question_number + 1)
 
 def completed(request, quiz_number):
+	quiz = Quiz.objects.get(quiz_number=quiz_number)
+	questions = quiz.questions.all()
+	saved_answers = request.session.get(quiz_number, {})
+	num_correct_answers = 0
+	for question_number, answer in saved_answers.items():
+		correct_answer = questions[int(question_number) - 1].correct
+		if correct_answer == answer:
+			num_correct_answers += 1
 	context = {
-	    	"correct": 12,
-	    	"total": 20,
-		"quiz_number": quiz_number,
-	}
+		"correct": num_correct_answers,
+		"total": questions.count(),
+		}
 	return render(request, "quiz/results.html", context)
